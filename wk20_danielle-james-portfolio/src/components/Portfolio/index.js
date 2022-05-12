6ed704d5a75c8b6bdb6b36751972217636598e1e
Danielle James <40706088+dosbor3@users.noreply.github.com>
2022-05-12 12:27:15 -0500

    Update index.js

--- wk20_danielle-james-portfolio/src/components/Portfolio/index.js
+++ wk20_danielle-james-portfolio/src/components/Portfolio/index.js
@@ -12,27 +12,27 @@ function Portfolio(props) {
         {
             name: 'AA Budget Tracker',
             description: 'PWA app that allows users to track finances with little to no internet connection',
-            src: '"{require(`../../assets/large/$1.png`)}"',
+            src: '"{require(`../../assets/large/img1.png`)}"',
             deploy:'https://pure-fjord-98425.herokuapp.com/',
             gitHub: 'https://github.com/dosbor3/AA-Budget-Tracker'
         },
         {
             name: 'DevelopIT Full Stack Forum',
             description: 'CMS-style blog site forum, where current and prospective developers can share inquiries and responses',
-            src: '"{require(`../../assets/large/$2.png`)}"',
+            src: '"{require(`../../assets/large/img2.png`)}"',
             deploy:'https://safe-river-61746.herokuapp.com/',
             gitHub: 'https://github.com/dosbor3/AA-Budget-Tracker'
         },
         {
             name: 'The Tech Blog',
             description: 'CMS style blog site where developers can publish their blog posts and comment on other developer\'s posts as well',
-            src: '"{require(`../../assets/large/$3.png`)}"',
+            src: '"{require(`../../assets/large/img3.png`)}"',
             deploy:'https://git.heroku.com/still-fjord-46453.git',
             gitHub: 'https://github.com/dosbor3/wk14_mvc-tech-blog'
         },
         {
             name: 'Handy Little Note Taker',
-            src: '"{require(`../../assets/large/$4.png`)}"',
+            src: '"{require(`../../assets/large/$img4.png`)}"',
             description: 'App that allows users to write and save notes, using Express.js backend technologies',
             deploy:'https://note-taker-dosbor3.herokuapp.com/',
             gitHub: 'https://github.com/dosbor3/wk11_note-taker'
@@ -52,7 +52,7 @@ function Portfolio(props) {
             <p>gitHub Link: <a href={project.gitHub}>{project.gitHub}</a></p>
             <div className="flex-row">
                 <img
-                    src={img1}
+                    src={projects.src}
                     alt={project.name}
                     className='img-thumbnail mx-1'
                     key={project.name}
