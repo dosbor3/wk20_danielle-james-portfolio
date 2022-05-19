036df7780e501012c778aff9cedf99770d6cbf2d
Danielle James <40706088+dosbor3@users.noreply.github.com>
2022-05-19 11:16:07 -0500

    Update index.js

--- wk20_danielle-james-portfolio/src/components/Portfolio/index.js
+++ wk20_danielle-james-portfolio/src/components/Portfolio/index.js
@@ -4,6 +4,8 @@ import img1 from '../../assets/large/1.png';
 import img2 from '../../assets/large/2.png';
 import img3 from '../../assets/large/3.png';
 import img4 from '../../assets/large/4.png';
+import img5 from '../../assets/large/5.jpg';
+import img6 from '../../assets/large/6.jpg';
 
 
 function Portfolio(props) {
@@ -12,31 +14,47 @@ function Portfolio(props) {
         {
             name: 'AA Budget Tracker',
             description: 'PWA app that allows users to track finances with little to no internet connection',
-            src: '"{require(`../../assets/large/$1.png`)}"',
+            src: img1,
             deploy:'https://pure-fjord-98425.herokuapp.com/',
             gitHub: 'https://github.com/dosbor3/AA-Budget-Tracker'
         },
         {
             name: 'DevelopIT Full Stack Forum',
             description: 'CMS-style blog site forum, where current and prospective developers can share inquiries and responses',
-            src: '"{require(`../../assets/large/$1.png`)}"',
+            src: img2,
             deploy:'https://safe-river-61746.herokuapp.com/',
             gitHub: 'https://github.com/dosbor3/AA-Budget-Tracker'
         },
         {
             name: 'The Tech Blog',
             description: 'CMS style blog site where developers can publish their blog posts and comment on other developer\'s posts as well',
-            src: '"{require(`../../assets/large/$1.png`)}"',
+            src: img3,
             deploy:'https://git.heroku.com/still-fjord-46453.git',
             gitHub: 'https://github.com/dosbor3/wk14_mvc-tech-blog'
         },
+        
         {
             name: 'Handy Little Note Taker',
-            src: '"{require(`../../assets/large/$1.png`)}"',
+            src: img4,
             description: 'App that allows users to write and save notes, using Express.js backend technologies',
             deploy:'https://note-taker-dosbor3.herokuapp.com/',
             gitHub: 'https://github.com/dosbor3/wk11_note-taker'
-        }
+        }, 
+        {
+            name: 'Password Generator',
+            description: 'Program that generates a password based on criteria selected by the user',
+            src: img5,
+            deploy:'https://dosbor3.github.io/wk3_password-generator/',
+            gitHub: 'https://github.com/dosbor3/wk3_password-generator'
+        },
+        {
+            name: 'Violetlattes',
+            description: 'An online store that will allow artist Kiorsten to sell her digital artwork.  This app was built with the followng technologies: HTML, CSS, Bootstrap, JavaScript, Node, React, Express, MongoDB, GraphQL, Apollo, JWT, StripeJs, Mongoose, Nodemon',
+            src: img6,
+            deploy:'https://sheltered-castle-99209.herokuapp.com/',
+            gitHub: 'https://github.com/DaniDelia253/violet-lattes'
+        },
+        
     ]);
 
     
@@ -52,7 +70,7 @@ function Portfolio(props) {
             <p>gitHub Link: <a href={project.gitHub}>{project.gitHub}</a></p>
             <div className="flex-row">
                 <img
-                    src={img1}
+                    src={project.src}
                     alt={project.name}
                     className='img-thumbnail mx-1'
                     key={project.name}
